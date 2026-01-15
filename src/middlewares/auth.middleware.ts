8d3c6fd3bc8062c4606396d00bba92ea91fa6119
rayan2228 <rayanh.dev@gmail.com>
2026-01-15 15:03:08 +0600

    feat : auth middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,32 @@
+import type { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { config } from "./../config/index";
+
+interface ExtendedRequest extends Request {
+  user: any;
+}
+export const authMiddleware = (
+  req: ExtendedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.JWT_SECRET!);
+    req.user = decoded;
+    next();
+  } catch {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
